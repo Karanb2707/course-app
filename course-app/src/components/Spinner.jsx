3162ee541b0e348fc0acd497c0041dcc9c8a5549
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-20 01:12:46 +0530

    Added Spinner css

--- course-app/src/components/Spinner.jsx
+++ course-app/src/components/Spinner.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div>
-        <h1>Loading</h1>
+    <div className="flex flex-col items-center justify-center h-40">
+      <div className="w-10 h-10 border-4 border-slate-600 border-t-transparent rounded-full animate-spin"></div>
+      <p className="mt-2 text-black font-semibold">Loading</p>
     </div>
   )
 }
