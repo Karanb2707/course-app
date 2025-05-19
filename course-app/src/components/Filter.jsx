194404c305d3a6fe596d2c5bded6baaf6cc0714c
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-05-19 10:44:15 +0000

    Button css

--- course-app/src/components/Filter.jsx
+++ course-app/src/components/Filter.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Filter = ({filterData}) => {
   return (
-    <div className='w-full flex items-center justify-center space-x-3 p-2 mx-auto'>
+    <div className='w-full flex items-center justify-center space-x-3 p-4 mx-auto'>
         {filterData.map(data => {
             return (
-                <button key={data.id} className='w-fit border px-4 py-2 rounded-xl bg-blue-900 text-white cursor-pointer'>
+                <button key={data.id} className='w-fit border px-4 py-2 rounded-xl font-semibold bg-blue-900 text-white cursor-pointer
+                  hover:bg-blue-300 hover:text-black'>
                     {data.title}  
                 </button>
             )
