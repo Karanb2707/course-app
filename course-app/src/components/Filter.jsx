d00eece2568c4cc1f15084eed80b1be6c515d2dd
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-05-19 10:21:34 +0000

    Added CSS

--- course-app/src/components/Filter.jsx
+++ course-app/src/components/Filter.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Filter = ({filterData}) => {
   return (
-    <div>
+    <div className='w-full flex items-center justify-center space-x-3 p-2 mx-auto'>
         {filterData.map(data => {
             return (
-                <button key={data.id}>
+                <button key={data.id} className='w-fit border px-4 py-2 rounded-xl bg-blue-900 text-white cursor-pointer'>
                     {data.title}  
                 </button>
             )
--- course-app/src/components/Navbar.jsx
+++ course-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='w-full text-center bg-slate-600 p-2 text-white text-xl font-bold'>
         <nav>
             <h1>Top Courses</h1>
         </nav>
